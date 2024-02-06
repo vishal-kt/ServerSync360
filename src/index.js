2f2744f835d78829436a6054490926d897b2c572
VISHAL <vishalstar895@gmail.com>
2024-02-06 22:48:11 +0530

    update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ dotenv.config({
 })
 
 
-connectDB()
-
+connectDB() 
+.then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running at port :{process.env.PORT}`);
+  })
+}) 
+.catch((err)=>{
+  console.log("MongoDB connection failed !!",err );
+})
 
 
 
