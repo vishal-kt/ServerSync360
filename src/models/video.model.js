dcd023178e9e2b658749d4c861c5e394f4b4f266
VISHAL <vishalstar895@gmail.com>
2024-02-07 17:58:48 +0530

    update

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,9 +1,50 @@
 import mongoose from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new mongoose.Schema({
-    
-},{timestamps:true})
+    videoFie:{
+        type:String, //cloudinary url
+        required:true
+    },
+    thumbnail:{
+        type:String, //cloudinary url
+        required:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    }
 
+},
+    {
+
+    timestamps:true
+    
+    }
+)
+
+
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const VideoSchema = mongoose.model("VideoSchema",videoSchema)
 
